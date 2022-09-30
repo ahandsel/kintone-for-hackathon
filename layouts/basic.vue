5eca1bc7e381d3acbb6f8972e41ff17568c23b05
Genji Fujimori <genji-fujimori@cybozu.co.jp>
2022-10-01 02:36:31 +0900

    Create basic.vue

---
+++ layouts/basic.vue
@@ -0,0 +1,30 @@
+<!--
+  Usage:
+
+```md
+---
+layout: basic
+---
+```
+-->
+
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div class="relative h-full w-full intro grid grid-cols-12">
+    <div class="absolute h-full w-full" style="z-index: -10">
+      <BubbleFrame />
+    </div>
+    <div class="col-span-12 m-auto">
+      <div class="text-center">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
