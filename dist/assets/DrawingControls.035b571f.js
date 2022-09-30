9be91dd394c77e65980354c1cacd6e4fa99cd87c
Genji Fujimori <genji-fujimori@cybozu.co.jp>
2022-10-01 03:29:33 +0900

    Slide build

---
+++ dist/assets/DrawingControls.035b571f.js
@@ -0,0 +1,1 @@
+import"./DrawingControls.vue_vue_type_script_setup_true_lang.5056b51f.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.5056b51f.js";import"./index.abb0921c.js";export{t as default};
---
+++ dist/assets/DrawingPreview.741f2674.js
@@ -0,0 +1,1 @@
+import"./index.abb0921c.js";import{ah as r}from"./index.abb0921c.js";export{r as default};
